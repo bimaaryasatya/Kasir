df210e1737b20576dc127f10e60231334776ce39
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2024-10-08 16:33:17 +0700

    Add Admin

---
+++ NetBeansProjects/kasir/src/main/java/com/mycompany/kasir/Profile.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kasir;
+
+/**
+ *
+ * @author BIMAZZNXT
+ */
+public class Profile {
+    private int id;
+    private String username;
+    private String password;
+    private int role_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+}
