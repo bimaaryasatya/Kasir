7cd14b204628838e4e2807ddc9a66d39fc915825
not-dika <yukimak420@gmail.com>
2024-12-24 16:14:11 +0700

    Update Database

--- kasir.sql
+++ kasir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 16, 2024 at 11:55 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2024 at 10:05 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `kategori` (
-  `id_kategori` int NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `kategori` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `kategori`
@@ -46,16 +46,16 @@ INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
 --
 
 CREATE TABLE `produk` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `kode` varchar(10) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `gambar` varchar(100) NOT NULL,
   `kategori` varchar(100) DEFAULT NULL,
   `suplier` varchar(100) DEFAULT NULL,
-  `harga_jual` int NOT NULL,
-  `harga_beli` int NOT NULL,
-  `stok` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `harga_jual` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `produk`
@@ -72,9 +72,9 @@ INSERT INTO `produk` (`id`, `kode`, `nama`, `gambar`, `kategori`, `suplier`, `ha
 --
 
 CREATE TABLE `suplier` (
-  `id_suplier` int NOT NULL,
+  `id_suplier` int(11) NOT NULL,
   `suplier` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `suplier`
@@ -86,15 +86,56 @@ INSERT INTO `suplier` (`id_suplier`, `suplier`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `total_harga` decimal(10,2) DEFAULT NULL,
+  `jumlah_bayar` decimal(10,2) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`id_transaksi`, `id_user`, `total_harga`, `jumlah_bayar`, `timestamp`) VALUES
+(1, 26, '130000.00', '130000.00', '2024-12-24 08:12:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_detail`
+--
+
+CREATE TABLE `transaction_detail` (
+  `id_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) DEFAULT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaction_detail`
+--
+
+INSERT INTO `transaction_detail` (`id_detail`, `id_transaksi`, `nama_barang`, `qty`) VALUES
+(1, 1, 'Router', 10);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `level` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -103,7 +144,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
 (11, 'bimazznxt', 'admin123', 'Owner'),
 (12, 'getCode', 'admin1234', 'Admin'),
-(26, 'hanif.com', 'admin12', 'Admin');
+(26, 'hanif.com', 'admin12', 'Kasir');
 
 --
 -- Indexes for dumped tables
@@ -122,6 +163,7 @@ ALTER TABLE `kategori`
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `kode` (`kode`),
+  ADD UNIQUE KEY `nama` (`nama`),
   ADD KEY `id_kategori` (`kategori`,`suplier`),
   ADD KEY `suplier` (`suplier`);
 
@@ -131,6 +173,21 @@ ALTER TABLE `produk`
 ALTER TABLE `suplier`
   ADD PRIMARY KEY (`id_suplier`),
   ADD UNIQUE KEY `suplier` (`suplier`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `transaction_detail`
+--
+ALTER TABLE `transaction_detail`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_transaksi` (`id_transaksi`,`nama_barang`),
+  ADD KEY `transaction_detail_ibfk_1` (`nama_barang`);
 
 --
 -- Indexes for table `user`
@@ -147,25 +204,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `suplier`
 --
 ALTER TABLE `suplier`
-  MODIFY `id_suplier` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_suplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transaction_detail`
+--
+ALTER TABLE `transaction_detail`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
@@ -177,6 +246,19 @@ ALTER TABLE `user`
 ALTER TABLE `produk`
   ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`kategori`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`suplier`) REFERENCES `suplier` (`suplier`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaction_detail`
+--
+ALTER TABLE `transaction_detail`
+  ADD CONSTRAINT `transaction_detail_ibfk_1` FOREIGN KEY (`nama_barang`) REFERENCES `produk` (`nama`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_detail_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `transaction` (`id_transaksi`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
